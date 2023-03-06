2c92748f34bfd4cc35b16b0d1adbf6226f4fa5ca
susovcekantonio <asusovcek0@gmail.com>
2023-03-06 09:59:14 +0100

    US5 : Added manual scoring method

--- src/main/java/com/hackathon/event/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/hackathon/event/service/impl/RegistrationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hackathon.event.service.impl;
 
 import com.hackathon.event.dto.RegistrationRequestDto;
+import com.hackathon.event.dto.ScoreRequestDto;
 import com.hackathon.event.mapper.RegistrationMapper;
 import com.hackathon.event.model.*;
 import com.hackathon.event.model.enumeration.SkillType;
@@ -8,6 +9,7 @@ import com.hackathon.event.repository.*;
 import com.hackathon.event.service.RegistrationService;
 import com.hackathon.event.util.ScoringEngine;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -20,6 +22,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final EventRepository eventRepository;
     private final RegistrationMapper registrationMapper;
     private final SkillRepository skillRepository;
+    private final CommentRepository commentRepository;
 
 
     @Override
@@ -27,7 +30,7 @@ public class RegistrationServiceImpl implements RegistrationService {
         ScoringEngine scoringEngine = new ScoringEngine();
         Integer score = scoringEngine.CalculateScore(registrationRequestDto);
 
-        Event event=eventRepository.findById(eventId).orElseThrow(()-> new EntityNotFoundException());
+        Event event = eventRepository.findById(eventId).orElseThrow(() -> new EntityNotFoundException());
         Registration registration = registrationMapper.toEntity(registrationRequestDto, event);
 
         Name name = registration.getPersonal().getName();
@@ -49,7 +52,7 @@ public class RegistrationServiceImpl implements RegistrationService {
 
         registrationRepository.save(registration);
 
-        for(Skill skill : registration.getExperience().getSkills()){
+        for (Skill skill : registration.getExperience().getSkills()) {
             skill.setExperience(experience);
             skillRepository.save(skill);
         }
@@ -58,15 +61,43 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     @Override
-    public void deleteById(Long eventId, Long registrationId){
+    public void deleteById(Long eventId, Long registrationId) {
 
         Event event = eventRepository.findById(eventId).orElseThrow
-                (()-> new EntityNotFoundException("Event not found"));
+                (() -> new EntityNotFoundException("Event not found"));
         Registration registration = registrationRepository.findById(registrationId).orElseThrow
-                (()-> new EntityNotFoundException("Registration not found"));
+                (() -> new EntityNotFoundException("Registration not found"));
 
         event.getRegistrations().remove(registration);
         registrationRepository.delete(registration);
 
+    }
+
+    @Override
+    public ResponseEntity<String> score(Long eventId, Long registrationId, ScoreRequestDto scoreRequestDto) {
+        Registration registration = registrationRepository.findById(registrationId).orElseThrow(() -> new EntityNotFoundException("Registration doesn't exist"));
+        if (scoreRequestDto.getScore().charAt(0) == '+') {
+            String valueText = scoreRequestDto.getScore().substring(1);
+            Integer updatedScore = registration.getScore();
+            updatedScore += Integer.parseInt(valueText);
+            registration.setScore(updatedScore);
+            registrationRepository.save(registration);
+        } else if (scoreRequestDto.getScore().charAt(0) == '-') {
+            String valueText = scoreRequestDto.getScore().substring(1);
+            Integer updatedScore = registration.getScore();
+            updatedScore -= Integer.parseInt(valueText);
+            registration.setScore(updatedScore);
+            registrationRepository.save(registration);
+        } else {
+            return ResponseEntity.badRequest().body("Bad format");
+        }
+
+        Comment comment = new Comment();
+        comment.setScore(scoreRequestDto.getScore());
+        comment.setComment(scoreRequestDto.getComment());
+        comment.setRegistration(registration);
+        commentRepository.save(comment);
+
+        return ResponseEntity.ok("Saved");
     }
 }
