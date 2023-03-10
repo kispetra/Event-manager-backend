65cdaf991c73c441c581c063a8f0399679ca6829
Stjepan Milardic <stjepan.milardic@ericsson.com>
2023-03-10 12:09:25 +0100

    merget main to us11

--- src/main/java/com/hackathon/event/model/Participant.java
+++ src/main/java/com/hackathon/event/model/Participant.java
@@ -2,10 +2,12 @@ package com.hackathon.event.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="participants")
